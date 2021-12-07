c5e9576f7548da715cc7497c5b4fc61c8c614a8b
SShipon <shamsulislamshipon2022@gmail.com>
2021-12-06 20:24:25 -1200

    this is hello

--- js/shipon.js
+++ js/shipon.js
@@ -1,42 +1,36 @@
-/* input-search  */
 const searchBook = () => {
     const SearchField = document.getElementById("input-search");
     const searchText = SearchField.value;
-  const url = `https://openlibrary.org/search.json?q=${searchText}`;
- fetch(url)
+    //console.log(searchText);
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    //console.log(url)
+     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data));
         SearchField.value = "";
 }
-/* Total result  */
 const totalResult = document.getElementById('total-result');
+
 const displaySearchResult = books => {
-   const result = `${books.numFound}`
-    function colorName(){
-        totalResult.style.color='white'
-        totalResult.style.margin='20px 0px'
-    }
-    /*  error Heading */
+
+    const result = `${books.numFound}`
     if(result==="0"){
         totalResult.innerText =`No result Found`
-        colorName()
     }
     else{
-        totalResult.innerText =`Total Search Result  `
-        colorName()
+        totalResult.innerText =`Total Search Result ${books.numFound}`
     }
+    //console.log(docs);
    const searchResult = document.getElementById('search-result');
    const book = books.docs;
 
-   /* searchResult */
-
     searchResult.textContent ='';
-    book.slice(0,30).map(doc => {
+    book.forEach(doc => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100 rounded">
-        <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top w-75 mx-auto mt-2" alt="...">
+        <div class="card h-100">
+        <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top w-75 mx-auto" alt="...">
         <div class="card-body">
           <h5 class="card-title">${doc.title}</h5>
           <p class="card-text">Author name: ${doc.author_name}</p>
